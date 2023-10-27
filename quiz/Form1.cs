0fcbdc5ac429249ac210fc6124a71a2c2cf64967
bobmeier <Avdiu.Shpat@bene-edu.ch>
2023-10-27 12:31:45 +0200

    Add project files.

---
+++ quiz/Form1.cs
@@ -0,0 +1,46 @@
+namespace quiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "current highscore \n " + 0;
+            label2.Text = "score \n" + 0;
+            button1.Text = "start game";
+            button2.Text = "rules";
+            pictureBox1.Image = Image.FromFile("pictures/a.jpg");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
