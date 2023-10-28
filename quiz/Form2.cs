020bd2e78c8d8ed2952793c252e761424ee8e239
Uglypr1nces <shpatavdiu4@gmail.com>
2023-10-28 17:09:49 +0200

    created a check answer function

--- quiz/Form2.cs
+++ quiz/Form2.cs
@@ -12,12 +12,12 @@ namespace quiz
 {
     public partial class Form2 : Form
     {
+        private int num1 = 3;
         public Form2()
         {
             InitializeComponent();
             button1.Text = "OK";
             button2.Text = "Understand";
-
             pictureBox1.Image = Image.FromFile("pictures/b.jpg");
         }
 
@@ -33,23 +33,26 @@ namespace quiz
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int num1 = 3;
-            while (num1 > 0)
+            while (true)
             {
-                num1--;
-                pictureBox1.Image = Image.FromFile("pictures/c.jpg");
-                progressBar1.Value = 30;
+                num1 = num1--;
                 if (num1 == 2)
+                {
+                    num1--;
+                    pictureBox1.Image = Image.FromFile("pictures/c.jpg");
+                    progressBar1.Value = 30;
+                }
+                else if (num1 == 1)
                 {
                     pictureBox1.Image = Image.FromFile("pictures/a.jpg");
                     progressBar1.Value = 75;
                 }
-                else if (num1 == 1)
+                else if (num1 == 0)
                 {
                     pictureBox1.Image = Image.FromFile("pictures/d.jpg");
                     progressBar1.Value = 100;
-
                 }
+            
             }
 
         }
--- quiz/Form3.cs
+++ quiz/Form3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,12 @@ namespace quiz
 {
     public partial class Form3 : Form
     {
+        private string answer = " ";
+        private string rightanswer = "answer1";
+
         public Form3()
         {
+
             InitializeComponent();
             button1.Text = "Answer 1";
             button2.Text = "Answer 2";
@@ -21,9 +26,19 @@ namespace quiz
             button4.Text = "Answer 3";
 
             string a = "blobb blobb? answer1 = blob blob, answer 2 = blobby blobby, answer 3 = blobby blob, answer4 = blobby blob blob";
-            TextBox textBox1 = new TextBox();
             textBox1.Text = a;
+        }
 
+        private void AnswerChecker()
+        {
+            if (answer == rightanswer) 
+            {
+                pictureBox1.Image = Image.FromFile("pictures/a.jpg");
+            }
+            else 
+            {
+                pictureBox1.Image = Image.FromFile("pictures/b.jpg");
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -33,22 +48,26 @@ namespace quiz
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            answer = "answer1";
+            AnswerChecker();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            answer = "answer2";
+            AnswerChecker();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            answer = "answer4";
+            AnswerChecker();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            answer = "answer3";
+            AnswerChecker();
         }
     }
 }
