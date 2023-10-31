d60168dc299e1a993b605de70bc0ea88ac5edf87
Uglypr1nces <shpatavdiu4@gmail.com>
2023-10-31 16:21:15 +0100

    more stuff

--- quiz/Form3.cs
+++ quiz/Form3.cs
@@ -16,30 +16,58 @@ namespace quiz
         private Random rnd = new Random();
         private int rightanswer = 0;
         private int answer = 0;
+        public int points = 0;
 
         public Form3()
         {
             InitializeComponent();
-            int a = rnd.Next(0,20);
-            int b = rnd.Next(0, 20);
+
+            int a = rnd.Next(-50,50);
+            int b = rnd.Next(-50, 50);
+            int c = rnd.Next(-25, 25);
+            int d = rnd.Next(-25, 25);
+            int e = rnd.Next(-25, 25);
+            int f = rnd.Next(-25, 25);
+            List<string> operators = new List<string>();
+            operators.Add("+");
+            operators.Add("-");
+            operators.Add("*");
+            operators.Add("/");
+
+            int r = rnd.Next(operators.Count);
+            string s = operators[r];
+
+
             rightanswer = a + b;
-            button1.Text = "Answer1";
+            button1.Text = "Answer 1";
             button2.Text = "Answer 2";
             button3.Text = "Answer 3";
-            button4.Text = "Answer 3";
-            textBox1.Text = "what is " + a + "plus " + b;
-
+            button4.Text = "Answer 4";
+            textBox1.Text = "what is " + a + s + b;
         }
-
         private void AnswerChecker()
         {
             if (answer == rightanswer) 
             {
-                pictureBox1.Image = Image.FromFile("pictures/a.jpg");
+                if (answer < -50 & answer > 50)
+                {
+                    points = points + 75;
+                }
+                else if (answer < -25 & answer > 25)
+                {
+                    points = points + 50;
+                }
+                else
+                {
+                    points = points + 25;
+                }
+
+                    pictureBox1.Image = Image.FromFile("pictures/a.jpg");
             }
             else 
             {
                 pictureBox1.Image = Image.FromFile("pictures/b.jpg");
+                points = points - 25;
             }
         }
 
